760e6f53d06e10c95e476469d14bc7afa014e394
Timoteo stifft <timoteostifft@gmail.com>
2022-03-21 20:42:10 -0300

    feature: create test cards component

---
+++ src/components/AppTestCard.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="container">
+  <div id="content">
+    teste
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AppTestCard',
+};
+</script>
+
+<style scoped lang="scss">
+  #container{
+    max-width: 920px;
+    padding: 0 1rem;
+
+    #content{
+      width: 100%;
+      height: 6rem;
+
+      margin: 3rem auto 0;
+
+      border: 3px solid #3a628d;
+      background: #e7eaee;
+      border-radius: 1rem;
+      box-shadow: rgba(0, 0, 0, 0.4) 0px 10px 20px 0px;
+
+      background: #e7eaee;
+
+    }
+  }
+</style>
