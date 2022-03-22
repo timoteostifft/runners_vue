378ac60b4e029aef77b511274ba5bde8b54e77ef
Timóteo Stifft <timoteostifft@gmail.com>
2022-03-21 22:13:18 -0300

    style: fix test cards reposivity

--- src/components/AppTestCard.vue
+++ src/components/AppTestCard.vue
@@ -14,22 +14,18 @@ export default {
 
 <style scoped lang="scss">
   #container{
-    max-width: 920px;
+    max-width: 790px;
+
     padding: 0 1rem;
+    margin: 0 auto 2rem;
 
     #content{
-      width: 100%;
       height: 6rem;
-
-      margin: 3rem auto 0;
 
       border: 3px solid #3a628d;
       background: #e7eaee;
       border-radius: 1rem;
       box-shadow: rgba(0, 0, 0, 0.4) 0px 10px 20px 0px;
-
-      background: #e7eaee;
-
     }
   }
 </style>
