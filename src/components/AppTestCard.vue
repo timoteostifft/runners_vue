955c09e830b8a8a7a5f40daae63f2adcba9ecdac
Timóteo Stifft <timoteostifft@gmail.com>
2022-03-23 05:07:09 -0300

    style: set test cards base style

--- src/components/AppTestCard.vue
+++ src/components/AppTestCard.vue
@@ -1,17 +1,17 @@
 <template>
 <div id="container">
   <div id="content">
-    <!-- <img alt="App logo" src="../assets/runner.png">
+    <img alt="App logo" src="../assets/runner.png">
     <div id="info">
-      <p>Data: <span>22/04/2022</span></p>
-      <p>Quilomêtros: <span>23km</span></p>
-      <p>Participantes: <span>20</span></p>
+      <p>Data: <span>20/04/2022</span></p>
+      <p>Extensão: <span>24 KM</span></p>
+      <p>Participantes: <span>42</span></p>
     </div>
     <nav>
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-    </nav> -->
+      <button></button>
+      <button></button>
+      <button></button>
+    </nav>
   </div>
 </div>
 </template>
@@ -26,27 +26,82 @@ export default {
   #container{
     width: 100%;
     max-width: 350px;
-    height: 2rem;
+    height: 8.75rem;
 
     margin: 0 20px;
 
     & + &{
-      margin-top: 30px;
+      margin-top: 55px;
     }
 
-    @media (min-width: 848px) {
+    &:last-child{
+        margin-bottom: 55px;
+      }
+
+    @media (max-width: 600px) {
+      margin: 0;
+    }
+
+    @media (min-width: 844px) {
       & + &{
-        margin-top: 30px;
+        margin-top: 55px;
       }
 
       &:first-child{
-        margin-top: 30px;
+        margin-top: 55px;
       }
     }
 
-    border: 1px solid red;
-
     #content{
+      width: 100%;
+      height: 100%;
+
+      display: flex;
+      flex-direction: row;
+
+      background: #e7eaee;
+      border: 3px solid #3a628d;
+      border-radius: 1rem;
+      box-shadow: rgba(0, 0, 0, 0.4) 0px 10px 20px 0px;
+
+      img{
+        height: 100%;
+        width: 5rem;
+
+        border-radius: 12px 0 0 12px;
+        border-right: 3px solid #3a628d;
+      }
+
+      #info{
+        margin-left: 12px;
+
+        display: flex;
+        flex-direction: column;
+
+        p{
+          color: #3a628d;
+          font-weight: 500;
+          font-size: 14px;
+        }
+      }
+
+      nav{
+        display: flex;
+        flex-direction: column;
+
+        margin: auto 2px auto auto;
+
+        button{
+          height: 35px;
+          width: 35px;
+
+          border-radius: 1rem;
+
+          & + button{
+            margin-top: 14px;
+          }
+        }
+      }
     }
   }
 </style>
