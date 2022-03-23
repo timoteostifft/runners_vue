2bf828b50563e15b226688d15d2590f5188c83c6
Timóteo Stifft <timoteostifft@gmail.com>
2022-03-23 04:32:19 -0300

    feature: create test cards container

---
+++ src/components/AppTestCardContainer.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="testsContainer">
+  <AppTestCard/>
+  <AppTestCard/>
+  <AppTestCard/>
+  <AppTestCard/>
+  <AppTestCard/>
+  <AppTestCard/>
+  <AppTestCard/>
+  <AppTestCard/>
+  <AppTestCard/>
+</div>
+</template>
+
+<script>
+import AppTestCard from './AppTestCard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppTestCard,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#testsContainer{
+  max-width: 840px;
+
+  margin: 0 auto;
+  padding: 0 2rem;
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  @media (min-width: 768px) {
+    justify-content: space-around;
+    }
+}
+</style>
