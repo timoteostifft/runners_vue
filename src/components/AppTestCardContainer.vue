9a6e797c671d5ce592c5404fc22d641c843fade4
Timóteo Stifft <timoteostifft@gmail.com>
2022-03-23 21:26:16 -0300

    feature: set axios tests fetch

--- src/components/AppTestCardContainer.vue
+++ src/components/AppTestCardContainer.vue
@@ -1,21 +1,39 @@
 <template>
 <div id="testsContainer">
-  <AppTestCard/>
-  <AppTestCard/>
-  <AppTestCard/>
-  <AppTestCard/>
-  <AppTestCard/>
+  <h5>{{tests}}</h5>
   <AppTestCard/>
 </div>
 </template>
 
 <script>
+import api from '../api';
 import AppTestCard from './AppTestCard.vue';
 
 export default {
   name: 'App',
   components: {
     AppTestCard,
+  },
+  data() {
+    return {
+      tests: [],
+    };
+  },
+  created() {
+    this.getTests();
+  },
+  methods: {
+    getTests() {
+      api
+        .get('/tests/listByResult')
+        .then((response) => {
+          this.tests = response.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
+    },
   },
 };
 </script>
