9bc7e57d7a796232dc126e1e9e42fa17f25db2fe
Timoteo stifft <timoteostifft@gmail.com>
2022-03-24 00:56:40 -0300

    feature: set tests array fetch display from api

--- src/components/AppTestCard.vue
+++ src/components/AppTestCard.vue
@@ -1,149 +1,118 @@
 <template>
-<div id="container">
-  <div id="content">
-    <img id="mainImage" alt="App logo" src="../assets/runner.png">
-    <div id="info">
-      <p>Data: <span>20/04/2022</span></p>
-      <p>Extensão: <span>24 KM</span></p>
-      <p>Participantes: <span>42</span></p>
-    </div>
-    <nav>
-      <button id="removeButton">
-        <img alt="Remove Icon" src="../assets/removeIcon.png">
-      </button>
-      <button id="listButton">
-        <img alt="List Icon" src="../assets/listIcon.png">
-      </button>
-      <button id="addButton">
-        <img alt="Register Icon" src="../assets/addIcon.png">
-      </button>
-    </nav>
+<div id="content">
+  <img id="mainImage" alt="App logo" src="../assets/runner.png">
+  <div id="info">
+    <p>Data: <span>{{test.date}}</span></p>
+    <p>Extensão: <span>{{test.type}} km</span></p>
+    <p>Participantes: <span>{{test.runners.length}}</span></p>
   </div>
+  <nav>
+    <button id="removeButton">
+      <img alt="Remove Icon" src="../assets/removeIcon.png">
+    </button>
+    <button id="listButton">
+      <img alt="List Icon" src="../assets/listIcon.png">
+    </button>
+    <button id="addButton">
+      <img alt="Register Icon" src="../assets/addIcon.png">
+    </button>
+  </nav>
 </div>
 </template>
 
 <script>
 export default {
   name: 'AppTestCard',
+  props: ['test'],
 };
 </script>
 
 <style scoped lang="scss">
-  #container{
-    width: 100%;
-    max-width: 350px;
-    height: 8.75rem;
+#content{
+  width: 100%;
+  height: 100%;
 
-    margin: 0 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
 
-    & + &{
-      margin-top: 40px;
+  background: #e7eaee;
+  border: 3px solid #3a628d;
+  border-radius: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 10px 20px 0px;
+
+  #mainImage{
+    height: 100%;
+    width: 5rem;
+
+    border-radius: 12px 0 0 12px;
+    border-right: 3px solid #3a628d;
+  }
+
+  #info{
+    margin-left: 12px;
+
+    display: flex;
+    flex-direction: column;
+
+    p{
+      color: #3a628d;
+      font-weight: 500;
+      font-size: 14px;
     }
+  }
 
-    &:first-child{
-      margin-top: 50px;
-    }
+  nav{
+    display: flex;
+    flex-direction: column;
 
-    &:last-child{
-      margin-bottom: 55px;
-    }
+    margin: 6px 8px 6px 10px;
 
-    @media (max-width: 600px) {
-      margin: 0;
-    }
+    @media (min-width: 360px) {
+    margin: 6px 8px 6px 30px;
+  }
 
-    @media (min-width: 844px) {
-      & + &{
-        margin-top: 50px;
-      }
+    button{
+      height: 35px;
+      width: 35px;
 
-      &:first-child{
-        margin-top: 50px;
-      }
-    }
-
-    #content{
-      width: 100%;
-      height: 100%;
-
-      display: flex;
-      align-items: center;
-      flex-direction: row;
-
-      background: #e7eaee;
-      border: 3px solid #3a628d;
+      border: none;
       border-radius: 1rem;
-      box-shadow: rgba(0, 0, 0, 0.4) 0px 10px 20px 0px;
 
-      #mainImage{
-        height: 100%;
-        width: 5rem;
-
-        border-radius: 12px 0 0 12px;
-        border-right: 3px solid #3a628d;
+      & + button{
+        margin-top: 14px;
       }
 
-      #info{
-        margin-left: 12px;
+      &:nth-child(1){
+        background: #E52e4d;
 
-        display: flex;
-        flex-direction: column;
-
-        p{
-          color: #3a628d;
-          font-weight: 500;
-          font-size: 14px;
+        img{
+          margin: 0 auto;
+          height: 18px;
+          width: auto;
         }
       }
 
-      nav{
-        display: flex;
-        flex-direction: column;
+      &:nth-child(2){
+        background: #46568f;
 
-        margin: 6px 8px 6px auto;
+        img{
+          margin: 0 auto;
+          height: 16px;
+          width: auto;
+        }
+      }
 
-        button{
-          height: 35px;
-          width: 35px;
+      &:nth-child(3){
+        background: #25bb75;
 
-          border: none;
-          border-radius: 1rem;
-
-          & + button{
-            margin-top: 14px;
-          }
-
-          &:nth-child(1){
-            background: #E52e4d;
-
-            img{
-              margin: 0 auto;
-              height: 18px;
-              width: auto;
-            }
-          }
-
-          &:nth-child(2){
-            background: #46568f;
-
-            img{
-              margin: 0 auto;
-              height: 16px;
-              width: auto;
-            }
-          }
-
-          &:nth-child(3){
-            background: #25bb75;
-
-            img{
-              margin: 0 auto;
-              height: 18px;
-              width: auto;
-            }
-          }
+        img{
+          margin: 0 auto;
+          height: 18px;
+          width: auto;
         }
       }
     }
   }
+}
 </style>
