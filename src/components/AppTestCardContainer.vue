10513d033869fdd29e9eceed5e2c55a066c9beaf
Timóteo Stifft <timoteostifft@gmail.com>
2022-03-27 21:44:05 -0300

    refactor: changed submit and modal handling responsability to parent component

--- src/components/AppTestCardContainer.vue
+++ src/components/AppTestCardContainer.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="mainContainer">
+    <header>
+      <h1>Listagem de <br /><span>Corridas</span></h1>
+      <button type="button" @click='handleUseModal'>
+        <img alt="Register Icon" src="../assets/addIcon.png" />
+      </button>
+
+      <AppTestForm v-show='isModalVisible' @close='handleUseModal' @submit='register'/>
+    </header>
     <div id="testsContainer" v-for="test in tests" :key="test.id">
-      <AppTestCard v-bind:test="test" @delete="getTests" />
+      <AppTestCard v-bind:test="test" @delete="getTests"/>
     </div>
   </div>
 </template>
@@ -9,15 +17,18 @@
 <script>
 import api from '../plugins/api';
 import AppTestCard from './AppTestCard.vue';
+import AppTestForm from './AppTestForm.vue';
 
 export default {
   name: 'App',
   components: {
     AppTestCard,
+    AppTestForm,
   },
   data() {
     return {
       tests: [],
+      isModalVisible: false,
     };
   },
   created() {
@@ -35,6 +46,22 @@ export default {
           console.log(error);
         });
     },
+    handleUseModal() {
+      this.isModalVisible = !this.isModalVisible;
+      this.getTests();
+    },
+    register(data) {
+      api
+        .delete('/tests/add/', data)
+        .then((response) => {
+          console.log(response.status);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      this.handleUseModal();
+    },
   },
 };
 </script>
@@ -42,9 +69,7 @@ export default {
 <style scoped lang="scss">
 #mainContainer {
   max-width: 840px;
-  margin: 0 auto;
-
-  padding: 0 auto;
+  margin: 2rem auto 0;
 
   display: flex;
   flex-wrap: wrap;
@@ -54,15 +79,52 @@ export default {
     justify-content: space-around;
   }
 
+  header{
+    padding: 0 1rem;
+
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+
+    align-items: center;
+
+    h1 {
+      color: #e7eaee;
+
+      span {
+        color: #4081c7;
+      }
+
+      @media (min-width: 640px) {
+        br {
+          display: none;
+        }
+      }
+    }
+
+    button {
+      height: 35px;
+      width: 35px;
+
+      margin-left: auto;
+
+      border: none;
+      border-radius: 1rem;
+      background: #25bb75;
+
+      img {
+        margin: 0 auto;
+        height: 18px;
+        width: auto;
+      }
+    }
+  }
+
   #testsContainer {
     margin-right: 2rem;
     margin-left: 2rem;
 
     margin-top: 30px;
-
-    // &:last-child{
-    //   margin-bottom: 60px;
-    // }
   }
 }
 </style>
