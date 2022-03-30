edd4c1d97b582faf4e72d810ca3c8b35a5ce848a
Timóteo Stifft <timoteostifft@gmail.com>
2022-03-30 02:56:58 -0300

    refactor: removed base modal style responsability from test for modal

--- src/components/AppTestForm.vue
+++ src/components/AppTestForm.vue
@@ -62,26 +62,6 @@ export default {
 </script>
 
 <style lang='scss'>
-  .modal-backdrop {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.25);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .modal {
-    width: 30rem;
-    padding: 1.5rem;
-
-    background: #FFFFFF;
-    box-shadow: rgba(0, 0, 0, 0.6) 0px 10px 20px 0px;
-
-    color: #3a628d;
 
     #header{
       display: flex;
@@ -187,5 +167,4 @@ export default {
       }
     }
 
-  }
 </style>
